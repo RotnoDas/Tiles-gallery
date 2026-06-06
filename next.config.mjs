539edf4956b5d04c63b65d399c7cda48e70e9927
Rotno Chandra Das <rotno050@gmail.com>
2026-06-07 03:17:14 +0600

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ["@better-auth/kysely-adapter"],
   },
