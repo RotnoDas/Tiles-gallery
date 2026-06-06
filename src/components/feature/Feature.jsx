4c1d8aa85bbdcca0c6753c1428e62b07d6a375d7
Rotno Chandra Das <rotno050@gmail.com>
2026-06-07 03:34:00 +0600

    Done

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -8,7 +8,7 @@ const Feature = async() => {
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 py-12 md:py-16">
             <h2 className="text-2xl md:text-3xl font-bold text-center md:text-left mb-6">Featured Tiles</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 animate__zoomIn">
                 {
                     featuredTiles.map((tiles) => {
                         return (
