e9fb35e3fa62c13bfab97a367f317af4287e46c8
Rotno Chandra Das <rotno050@gmail.com>
2026-06-07 06:12:12 +0600

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/banner/Banner";
 import Feature from "@/components/feature/Feature";
+import Marquee from "@/components/marquee/Marquee";
 
 export default function Home() {
     return (
         <div>
             <Banner></Banner>
+            <Marquee></Marquee>
             <Feature></Feature>
         </div>
     )
